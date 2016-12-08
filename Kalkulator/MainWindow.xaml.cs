d3302ddbc4a8dcb0ab4f9d26b160ac91fb4196d7
Mar3q <notyzopn@gmail.com>
2016-12-08 15:29:39 +0100

    Wstepna logika poprawaiony commit

--- Kalkulator/MainWindow.xaml.cs
+++ Kalkulator/MainWindow.xaml.cs
@@ -18,6 +18,17 @@ namespace Kalkulator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    enum Działanie
+    {
+        brak = 0,
+        dodawanie,
+        odejmowanie,
+        mnozenie,
+        dzielenie,
+        wynik
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -43,6 +54,109 @@ namespace Kalkulator
         {
              MessageBox.Show("Nazwa programu: Kalkulator \nAutorzy: Mariusz Bubrowski i Marek Dąbrowski");
         }
+        private Działanie OstatnioWybraneDzialanie = Działanie.brak;
+        
+        private void liczba_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+            if (Działanie.wynik == OstatnioWybraneDzialanie)
+            {
+                Wynik.Text = string.Empty;
+                OstatnioWybraneDzialanie = Działanie.brak;
+            }
+            Button oButton = (Button)oSender;
+            Wynik.Text += oButton.Content;
+        }
+
+        private void przecinek_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+            if (Działanie.wynik == OstatnioWybraneDzialanie)
+            {
+                Wynik.Text = string.Empty;
+                OstatnioWybraneDzialanie = Działanie.brak;
+            }
+            if ((Wynik.Text.Contains(',')) ||
+                (0 == Wynik.Text.Length))
+            {
+                return;
+            }
+            Wynik.Text += ",";
+        }
+
+        private void czyszczenie_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+            Wynik.Text = string.Empty;
+            Pamiec.Text = string.Empty;
+            Operacja.Text = string.Empty;
+            OstatnioWybraneDzialanie = Działanie.brak;
+        }
+
+        private void działanie_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+
+            if ((Działanie.brak != OstatnioWybraneDzialanie) || (Działanie.wynik != OstatnioWybraneDzialanie))
+            {
+                wynik_Click(this, eRoutedEventArgs);
+            }
+            Button oButton = (Button)oSender;
+            switch (oButton.Content.ToString())
+            {
+                case "+":
+                    OstatnioWybraneDzialanie = Działanie.dodawanie;
+                    break;
+                case "-":
+                    OstatnioWybraneDzialanie = Działanie.odejmowanie;
+                    break;
+                case "*":
+                    OstatnioWybraneDzialanie = Działanie.mnozenie;
+                    break;
+                case "/":
+                    OstatnioWybraneDzialanie = Działanie.dzielenie;
+                    break;
+                default:
+                    MessageBox.Show("Nieznana operacja!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+            }
+            Pamiec.Text = Wynik.Text;
+            Operacja.Text = oButton.Content.ToString();
+            Wynik.Text = string.Empty;
+        }
+
+        private void wynik_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
+        {
+            // Nie wykonywano operacji, nie można wyliczyć wyniku
+            if ((Działanie.wynik == OstatnioWybraneDzialanie) ||
+                (Działanie.brak == OstatnioWybraneDzialanie))
+            {
+                return;
+            }
+            if (string.IsNullOrEmpty(Wynik.Text))
+            {
+                Wynik.Text = "0";
+            }
+            switch (OstatnioWybraneDzialanie)
+            {
+                case Działanie.dodawanie:
+                    Wynik.Text = (double.Parse(Pamiec.Text) +
+                        double.Parse(Wynik.Text)).ToString();
+                    break;
+                case Działanie.odejmowanie:
+                    Wynik.Text = (double.Parse(Pamiec.Text) -
+                        double.Parse(Wynik.Text)).ToString();
+                    break;
+                case Działanie.mnozenie:
+                    Wynik.Text = (double.Parse(Pamiec.Text) *
+                        double.Parse(Wynik.Text)).ToString();
+                    break;
+                case Działanie.dzielenie:
+                    Wynik.Text = (double.Parse(Pamiec.Text) /
+                        double.Parse(Wynik.Text)).ToString();
+                    break;
+            }
+            OstatnioWybraneDzialanie = Działanie.wynik;
+            Operacja.Text = string.Empty;
+            Pamiec.Text = string.Empty;
+        }
+
 
     }
 }
